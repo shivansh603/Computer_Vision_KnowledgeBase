49843cbf90fa0aa37b76e022709ddc2186063018
shivansh603 <102960901+shivansh603@users.noreply.github.com>
2024-05-17 16:21:53 +0530

    Update inference.py

--- Optical Flow/Lucas_Kanade_Sparse_Optical_Flow/inference.py
+++ Optical Flow/Lucas_Kanade_Sparse_Optical_Flow/inference.py
@@ -3,7 +3,7 @@ import cv2
 
 cap = cv2.VideoCapture('sample.mp4') 
 
-# params for corner detection 
+# Params for corner detection 
 feature_params = dict( maxCorners = 100, 
 					qualityLevel = 0.3, 
 					minDistance = 7, 
