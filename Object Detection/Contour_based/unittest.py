7c9fa473a062babbb76b48f1d00c5e0ab6bd428f
vamshi <vamshi.gurram@techolution.com>
2024-05-24 16:40:38 +0530

    added unit test file

--- Object Detection/Contour_based/unittest.py
+++ Object Detection/Contour_based/unittest.py
@@ -1,1 +1,11 @@
-#
+from inference import ObjectDetector
+import cv2 
+
+
+
+
+
+if __name__ == "__main__":
+    detector = ObjectDetector()
+    image_path = "Sample_images/1.png"
+    image = cv2.imread(image_path)
