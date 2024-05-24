9197ffde945c7291a67a2a856daebc95a379f980
vamshi <vamshi.gurram@techolution.com>
2024-05-24 16:45:54 +0530

    re

--- Object Detection/Contour_based/inference.py
+++ Object Detection/Contour_based/inference.py
@@ -95,7 +95,7 @@ class ObjectDetector:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Object detection in images using edge detection and contour analysis.")
-    parser.add_argument("image_path", type=str, help="Path to the input image.")
+    parser.add_argument("--image_path", type=str, help="Path to the input image.")
     args = parser.parse_args()
 
     detector = ObjectDetector()
