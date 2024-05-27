0d5ad7c719d6e8308d1183f9286e5b4cb95a0ad2
nisarg.doshi <nisarg.doshi@techolution.com>
2024-05-27 19:07:55 +0530

    Updated HE using cv2 instead of other library and fixed other bugs

--- Image Enhancement/Histogram Equalization/inference.py
+++ Image Enhancement/Histogram Equalization/inference.py
@@ -1,56 +1,40 @@
-import cv2
 import argparse
-from image_enhancement import image_enhancement
+import cv2
+import os
 
 
-class ImageEnhancer:
-    """
-    Class to enhance images using various methods.
-    """
+class ImageProcessor:
+    def __init__(self, image_path, output_path):
+        self.image_path = image_path
+        self.output_path = output_path
+        self.image = None
+        self.equalized_image = None
 
-    def __init__(self, image):
-        """
-        Initializes the ImageEnhancer object with the input image.
-        
-        Args:
-            image (numpy.ndarray): Input image.
-        """
-        self.image = image
+    def read_image(self):
+        self.image = cv2.imread(self.image_path, 0)
 
-    def enhance_image(self, method='GHE'):
-        """
-        Enhances the input image using the specified method.
-        
-        Args:
-            method (str): Name of the enhancement method. Default is 'BBHE'.
-        
-        Returns:
-            numpy.ndarray: Enhanced image.
-        """
-        ie = image_enhancement.IE(self.image, 'RGB')
-        if method == 'GHE':
-            return ie.GHE()
-        # elif method == 'CLAHE':
-        #     return ie.CLAHE()
+    def equalize_histogram(self):
+        if self.image is not None:
+            self.equalized_image = cv2.equalizeHist(self.image)
         else:
-            raise ValueError("Invalid enhancement method. Supported methods: 'GHE'")
+            print("Please read an image first.")
 
-
+    def write_image(self):
+        if self.equalized_image is not None:
+            cv2.imwrite(self.output_path, self.equalized_image)
+        else:
+            print("No equalized image to write. Please equalize the histogram first.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Image Enhancement")
-    parser.add_argument("input_image", type=str, help="Path to the input image")
-    parser.add_argument("output_image", type=str, help="Path to save the enhanced image")
-    parser.add_argument("--method", type=str, default="GHE", choices=["BBHE", "CLAHE"],
-                        help="Enhancement method to use (default: GHE)")
+    parser = argparse.ArgumentParser(description="Histogram Equalization")
+    parser.add_argument("image_path", type=str, help="Path to the input image")
+    parser.add_argument("output_path", type=str, help="Path to save the output image")
     args = parser.parse_args()
 
-    input_image = cv2.imread(args.input_image)
+    image_processor = ImageProcessor(args.image_path, args.output_path)
+    image_processor.read_image()
+    image_processor.equalize_histogram()
+    image_processor.write_image()
 
-    image_enhancer = ImageEnhancer(input_image)
-    enhanced_image = image_enhancer.enhance_image(method=args.method)
-
-    cv2.imwrite(args.output_image, enhanced_image)
-
-
+    print(f"Equalized image saved to {args.output_path}")
